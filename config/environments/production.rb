c167fce9ed3cef64e4369d7a1a355951da569275
Lucius Choi <lucius.choi@gmail.com>
2017-04-12 17:20:53 +0900

    setting heroku redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = 'wss://pure-sands-40528.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+      'https://pure-sands-40528.herokuapp.com' ]
 end
